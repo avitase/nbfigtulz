fe25445b3b2153622232fdbaf9c4ea06390358c7
Nis Meinert <nis.meinert@gmail.com>
2022-01-17 14:22:36 +0100

    Reformat file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,6 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#
 
 import re
 import sys
@@ -55,7 +54,6 @@ exclude_patterns = []
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
 html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
